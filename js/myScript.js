1bbf0ef25a48a3ce5e85f86acfcaa2f07ba08190
denvolk <denvolk242@gmail.com>
2022-05-11 18:20:27 +0300

    Code refactoring
    Add img-container for HTML, refactor CSS

--- js/myScript.js
+++ js/myScript.js
@@ -11,18 +11,24 @@ document.addEventListener("DOMContentLoaded", function changeColorAndHash(){
     document.location.hash = "#eng";
 });
 
-fetch("https://raw.githubusercontent.com/denvolk/frontend/test-branch/json/text.json")    //Для GitHub Pages
-//fetch("http://localhost:63342/frontend/json/text.json")   //Для локального использования
+//fetch("https://raw.githubusercontent.com/denvolk/frontend/test-branch/json/text.json")    //Для GitHub Pages
+fetch("http://localhost:63342/frontend/json/text.json")   //Для локального использования
     .then(response => response.json())
     .then(data => text = data);
 
 function changeLanguage(lang)   {
-    if (window.location.hash)   {
+    /*if (window.location.hash)   {
+        if (window.location.hash === "#ru" && lang === "ru")
+            return;
+        else if (window.location.hash === "#eng" && lang === "eng")
+            return;
+
         window.location.hash = lang;
     }
     else    {
         alert("Hash not set!");
         window.location.hash = lang;
+        return;
     }
 
     if (window.location.hash === "#ru") {
@@ -31,82 +37,103 @@ function changeLanguage(lang)   {
         SELECTOR_ENG.style.color = "#828282";
         document.documentElement.setAttribute("lang", "ru");
     }
-    else if (window.location.hash === "#eng") {
+    else if (window.location.hash === "#eng" && lang === "eng") {
         setTextToEng();
         SELECTOR_RU.style.color = "#828282";
         SELECTOR_ENG.style.color = "#070707";
         document.documentElement.setAttribute("lang", "eng");
+    }*/
+
+    if (window.location.hash) {
+        if (window.location.hash === "#ru" && lang === "eng")   {
+            setTextToEng();
+            SELECTOR_RU.style.color = "#828282";
+            SELECTOR_ENG.style.color = "#070707";
+            document.documentElement.setAttribute("lang", "eng");
+            window.location.hash = lang;
+        }
+        else if (window.location.hash === "#eng" && lang === "ru")  {
+            setTextToRu();
+            SELECTOR_RU.style.color = "#070707";
+            SELECTOR_ENG.style.color = "#828282";
+            document.documentElement.setAttribute("lang", "ru");
+            window.location.hash = lang;
+        }
+    }
+    else {
+        alert("Hash not set!");
+        window.location.hash = lang;
     }
 }
 
 function setTextToRu()  {
-    document.getElementById("span-menu-home").textContent = text["ru"]["homeVar"];
-    document.getElementById("span-menu-about").textContent = text["ru"]["aboutMeVar"];
-    document.getElementById("span-menu-skills").textContent = text["ru"]["skillsVar"];
-    document.getElementById("span-menu-portfolio").textContent = text["ru"]["portfolioVar"];
-    document.getElementById("span-menu-contacts").textContent = text["ru"]["contactsVar"];
+    document.getElementById("menu-home").textContent = text["ru"]["homeVar"];
+    document.getElementById("menu-about").textContent = text["ru"]["aboutMeVar"];
+    document.getElementById("menu-skills").textContent = text["ru"]["skillsVar"];
+    document.getElementById("menu-portfolio").textContent = text["ru"]["portfolioVar"];
+    document.getElementById("menu-contacts").textContent = text["ru"]["contactsVar"];
 
-    document.getElementById("span-first-name").textContent = text["ru"]["firstNameVar"];
-    document.getElementById("span-last-name").textContent = text["ru"]["lastNameVar"];
+    document.getElementById("first-name").textContent = text["ru"]["firstNameVar"];
+    document.getElementById("last-name").textContent = text["ru"]["lastNameVar"];
 
-    document.getElementById("span-info-first").textContent = text["ru"]["infoVarFirst"];
-    document.getElementById("span-info-second").textContent = text["ru"]["infoVarSecond"];
+    document.getElementById("info-first").textContent = text["ru"]["infoVarFirst"];
+    document.getElementById("info-second").textContent = text["ru"]["infoVarSecond"];
 
-    document.getElementById("span-about").textContent = text["ru"]["aboutMeVar"];
-    document.getElementById("span-about-p1-first").textContent = text["ru"]["aboutp1VarFirst"];
-    document.getElementById("span-about-p1-second").textContent = text["ru"]["aboutp1VarSecond"];
-    document.getElementById("span-about-p2-first").textContent = text["ru"]["aboutp2VarFirst"];
-    document.getElementById("span-about-p2-second").textContent = text["ru"]["aboutp2VarSecond"];
-    document.getElementById("span-about-p3-first").textContent = text["ru"]["aboutp3VarFirst"];
-    document.getElementById("span-about-p3-second").textContent = text["ru"]["aboutp3VarSecond"];
+    document.getElementById("about-h").textContent = text["ru"]["aboutMeVar"];
+    document.getElementById("about-p1-first").textContent = text["ru"]["aboutp1VarFirst"];
+    document.getElementById("about-p1-second").textContent = text["ru"]["aboutp1VarSecond"];
+    document.getElementById("about-p2-first").textContent = text["ru"]["aboutp2VarFirst"];
+    document.getElementById("about-p2-second").textContent = text["ru"]["aboutp2VarSecond"];
+    document.getElementById("about-p3-first").textContent = text["ru"]["aboutp3VarFirst"];
+    document.getElementById("about-p3-second").textContent = text["ru"]["aboutp3VarSecond"];
 
-    document.getElementById("span-skills").textContent = text["ru"]["skillsVar"];
-    document.getElementById("span-skills-p").textContent = text["ru"]["skillsPVar"];
+    document.getElementById("skills-h").textContent = text["ru"]["skillsVar"];
+    document.getElementById("skills-p").textContent = text["ru"]["skillsPVar"];
 
-    document.getElementById("span-portfolio").textContent = text["ru"]["portfolioVar"];
+    document.getElementById("portfolio-h").textContent = text["ru"]["portfolioVar"];
 
-    document.getElementById("span-contacts").textContent = text["ru"]["contactsVar"];
-    document.getElementById("span-contacts-p-first").textContent = text["ru"]["contactsPVarFirst"];
-    document.getElementById("span-contacts-p-second").textContent = text["ru"]["contactsPVarSecond"];
+    document.getElementById("contacts-h").textContent = text["ru"]["contactsVar"];
+    document.getElementById("contacts-p-first").textContent = text["ru"]["contactsPVarFirst"];
+    document.getElementById("contacts-p-second").textContent = text["ru"]["contactsPVarSecond"];
 
-    document.getElementById("span-button").textContent = text["ru"]["buttonVar"];
-    document.getElementById("span-footer-first").textContent = text["ru"]["footerVarFirst"];
-    document.getElementById("span-footer-second").textContent = text["ru"]["footerVarSecond"];
+    document.getElementById("button").textContent = text["ru"]["buttonVar"];
+    document.getElementById("footer-first").textContent = text["ru"]["footerVarFirst"];
+    document.getElementById("footer-second").textContent = text["ru"]["footerVarSecond"];
 }
 
 function setTextToEng() {
-    document.getElementById("span-menu-home").textContent = text["eng"]["homeVar"];
-    document.getElementById("span-menu-about").textContent = text["eng"]["aboutMeVar"];
-    document.getElementById("span-menu-skills").textContent = text["eng"]["skillsVar"];
-    document.getElementById("span-menu-portfolio").textContent = text["eng"]["portfolioVar"];
-    document.getElementById("span-menu-contacts").textContent = text["eng"]["contactsVar"];
+    document.getElementById("menu-home").textContent = text["eng"]["homeVar"];
+    document.getElementById("menu-about").textContent = text["eng"]["aboutMeVar"];
+    document.getElementById("menu-skills").textContent = text["eng"]["skillsVar"];
+    document.getElementById("menu-portfolio").textContent = text["eng"]["portfolioVar"];
+    document.getElementById("menu-contacts").textContent = text["eng"]["contactsVar"];
 
-    document.getElementById("span-first-name").textContent = text["eng"]["firstNameVar"];
-    document.getElementById("span-last-name").textContent = text["eng"]["lastNameVar"];
+    document.getElementById("first-name").textContent = text["eng"]["firstNameVar"];
+    document.getElementById("last-name").textContent = text["eng"]["lastNameVar"];
 
-    document.getElementById("span-info-first").textContent = text["eng"]["infoVarFirst"];
-    document.getElementById("span-info-second").textContent = text["eng"]["infoVarSecond"];
+    document.getElementById("info-first").textContent = text["eng"]["infoVarFirst"];
+    document.getElementById("info-second").textContent = text["eng"]["infoVarSecond"];
 
-    document.getElementById("span-about").textContent = text["eng"]["aboutMeVar"];
-    document.getElementById("span-about-p1-first").textContent = text["eng"]["aboutp1VarFirst"];
-    document.getElementById("span-about-p1-second").textContent = text["eng"]["aboutp1VarSecond"];
-    document.getElementById("span-about-p2-first").textContent = text["eng"]["aboutp2VarFirst"];
-    document.getElementById("span-about-p2-second").textContent = text["eng"]["aboutp2VarSecond"];
-    document.getElementById("span-about-p3-first").textContent = text["eng"]["aboutp3VarFirst"];
-    document.getElementById("span-about-p3-second").textContent = text["eng"]["aboutp3VarSecond"];
+    document.getElementById("about-h").textContent = text["eng"]["aboutMeVar"];
+    document.getElementById("about-p1-first").textContent = text["eng"]["aboutp1VarFirst"];
+    document.getElementById("about-p1-second").textContent = text["eng"]["aboutp1VarSecond"];
+    document.getElementById("about-p2-first").textContent = text["eng"]["aboutp2VarFirst"];
+    document.getElementById("about-p2-second").textContent = text["eng"]["aboutp2VarSecond"];
+    document.getElementById("about-p3-first").textContent = text["eng"]["aboutp3VarFirst"];
+    document.getElementById("about-p3-second").textContent = text["eng"]["aboutp3VarSecond"];
 
-    document.getElementById("span-skills").textContent = text["eng"]["skillsVar"];
-    document.getElementById("span-skills-p").textContent = text["eng"]["skillsPVar"];
+    document.getElementById("skills-h").textContent = text["eng"]["skillsVar"];
+    document.getElementById("skills-p").textContent = text["eng"]["skillsPVar"];
 
-    document.getElementById("span-portfolio").textContent = text["eng"]["portfolioVar"];
+    document.getElementById("portfolio-h").textContent = text["eng"]["portfolioVar"];
 
-    document.getElementById("span-contacts").textContent = text["eng"]["contactsVar"];
-    document.getElementById("span-contacts-p-first").textContent = text["eng"]["contactsPVarFirst"];
-    document.getElementById("span-contacts-p-second").textContent = text["eng"]["contactsPVarSecond"];
+    document.getElementById("contacts-h").textContent = text["eng"]["contactsVar"];
+    document.getElementById("contacts-p-first").textContent = text["eng"]["contactsPVarFirst"];
+    document.getElementById("contacts-p-second").textContent = text["eng"]["contactsPVarSecond"];
 
-    document.getElementById("span-button").textContent = text["eng"]["buttonVar"];
-    document.getElementById("span-footer-first").textContent = text["eng"]["footerVarFirst"];
-    document.getElementById("span-footer-second").textContent = text["eng"]["footerVarSecond"];
+    document.getElementById("button").textContent = text["eng"]["buttonVar"];
+    document.getElementById("footer-first").textContent = text["eng"]["footerVarFirst"];
+    document.getElementById("footer-second").textContent = text["eng"]["footerVarSecond"];
 }
 
 function scrollToPoint(point)   {
